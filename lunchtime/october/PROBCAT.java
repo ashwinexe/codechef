0c0d63cfff7e5c61f91965066f5dfc761bd80300
Ashwin Kumar Uppala <53075480+ashwinexe@users.noreply.github.com>
2021-10-27 19:39:24 +0530

    OCT lunchtime

---
+++ lunchtime/october/PROBCAT.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class PROBCAT
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+        while(t-- > 0)
+        {
+            int x = s.nextInt();
+            if(x >= 1 && x < 100) System.out.println("Easy");
+            else if (x >= 100 && x < 200) System.out.println("Medium");
+            else System.out.println("Hard");
+        }
+	}
+}
